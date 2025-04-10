2793fee33da5f055aaf41946777bf05f91ceb7b4
Serhii Frolov <cptneemoo@gmail.com>
2025-04-10 19:57:00 +0200

    first real lecture

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Mark';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+if (firstName !== 'Mark') {
+    console.log(firstName);
+}
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+console.log(myFirstJob);
+console.log(myCurrentJob);
