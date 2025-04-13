87b58ff1e638781b64395ea9732d1ce5bbb5ccb5
Serhii Frolov <cptneemoo@gmail.com>
2025-04-13 14:18:39 +0200

    finish part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -82,12 +82,12 @@ console.log(2 + 3 * 4 ** 2 - 5); // 45 (2 + (3 * (4 ** 2)) - 5)
 console.log(2 + 3 * (4 ** 2 - 5)); // 2 + 3 * (16 - 5) = 2 + 3 * 11 = 35
 // Strings and Template Literals
 const teacherName = 'Mark';
-const job = 'teacher';
+const personJob = 'teacher';
 const year = 1991;
 
-const Jonas = "I'm " + teacherName + ', a ' + (2025 - year) + ' years old ' + job + '!';
+const Jonas = "I'm " + teacherName + ', a ' + (2025 - year) + ' years old ' + personJob + '!';
 console.log(Jonas);
-const JonasNew = `I'm ${teacherName}, a ${2025 - year} years old ${job}!`;
+const JonasNew = `I'm ${teacherName}, a ${2025 - year} years old ${personJob}!`;
 console.log(JonasNew);
 console.log(`String with \n\
 multiple \n\
@@ -106,3 +106,118 @@ else {
     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
 // Type Conversion and Coercion
+console.log('I am ' + 23 + ' years old'); // Type coercion
+console.log('23' - '10' - 3); // 10 (string to number conversion)
+console.log('23' * '2'); // 46 (string to number conversion)
+console.log('23' / '2'); // 11.5 (string to number conversion)
+console.log('23' > '18'); // true (string comparison)
+Boolean(0); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('')); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean('Hello')); // true (non-empty string is truthy)
+console.log(Boolean(23)); // true (non-zero number is truthy)
+console.log(Boolean([])); // true (empty array is truthy)
+console.log(Boolean([1, 2, 3])); // true (non-empty array is truthy)
+console.log(Boolean({ name: 'Mark' })); // true (object is truthy)
+console.log(Boolean({})); // true (empty object is truthy)
+console.log(Boolean('false')); // true (non-empty string is truthy)
+console.log(Boolean('0')); // true (non-empty string is truthy)
+// Equality Operators: == vs. ===
+console.log('18' == 18); // true (loose equality)
+console.log('18' === 18); // false (strict equality)
+console.log(0 == false); // true (loose equality)
+console.log(0 === false); // false (strict equality)
+console.log(1 == true); // true (loose equality)
+console.log(1 === true); // false (strict equality)
+console.log('1' == true); // true (loose equality)
+console.log('1' === true); // false (strict equality)
+console.log(null == undefined); // true (loose equality)
+console.log(null === undefined); // false (strict equality)
+console.log('0' == false); // true (loose equality)
+console.log('0' === false); // false (strict equality)
+console.log('' == false); // true (loose equality)
+console.log('' === false); // false (strict equality)
+//Boolean Logic
+console.log(true && true); // true (AND)
+console.log(true && false); // false (AND)
+console.log(false && false); // false (AND)
+console.log(true || true); // true (OR)
+console.log(true || false); // true (OR)
+console.log(false || false); // false (OR)
+console.log(!true); // false (NOT)
+console.log(!false); // true (NOT)
+console.log(!0); // true (NOT)
+console.log(!''); // true (NOT)
+console.log(!undefined); // true (NOT)
+console.log(!null); // true (NOT)
+console.log(!NaN); // true (NOT)
+console.log(!1); // false (NOT)
+console.log(!'Hello'); // false (NOT)
+console.log(!'false'); // false (NOT)
+console.log(!'0'); // false (NOT)
+console.log(![]); // false (NOT)
+console.log(![1, 2, 3]); // false (NOT)
+console.log(!{}); // false (NOT)
+console.log(!{ name: 'Mark' }); // false (NOT)
+console.log(!function () { }); // false (NOT)
+console.log(!function () { return true; }); // false (NOT)
+console.log(!function () { return false; }); // false (NOT)
+console.log(!function () { return 0; }); // false (NOT)
+// The switch Statement
+// The switch statement is a cleaner way to write multiple if-else statements
+// The switch statement evaluates the expression and compares it with each case
+const day = 'monday';
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend! 😎');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+// If example for the switch statement
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+}
+else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+}
+else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+}
+else if (day === 'friday') {
+    console.log('Record videos');
+}
+else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend! 😎');
+}
+else {
+    console.log('Not a valid day!');
+}
+// Statements and Expressions
+// Expression: a piece of code that produces a value
+// Statement: a piece of code that performs an action
+// Example of an expression: 3 + 4 (produces a value)
+// Example of a statement: if (age >= 18) { console.log('Adult'); } (performs an action)
+// The Conditional (Ternary) Operator
+const drinkingAge = 23;
+const isAllowedToDrink = ageJonas >= drinkingAge ? 'Yes' : 'No';
+console.log(isAllowedToDrink); // Yes (if ageJonas is greater than or equal to drinkingAge, it will print 'Yes', otherwise 'No')
+console.log(`Is Jonas allowed to drink? ${isAllowedToDrink}`); // Is Jonas allowed to drink? Yes (if ageJonas is greater than or equal to drinkingAge, it will print 'Yes', otherwise 'No')
