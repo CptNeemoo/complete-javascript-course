c135bf6b693eefee1a2230a16d23c19ef2167f1b
Serhii Frolov <cptneemoo@gmail.com>
2025-04-13 14:27:00 +0200

    use strict

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true; // Variable assignment
+if (hasDriversLicense) console.log('I can drive!');
+// const interface = 'Audio'; // Keyword cannot be used as variable name
+// Functions
